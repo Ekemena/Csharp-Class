980afc5870e053ecef9b23e113f431c31a1af725
Dlaw85 <Dlaw85@DESKTOP-DS2G2IK>
2023-07-17 01:06:58 +0100

    Add project files.

---
+++ Giraffe1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Giraffe1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to my house");
+
+            //My first program on c# learning how to write c# program
+            /* 
+             This is a multi line comment on c#
+             thi part of the code is not executed 
+             yes i work here today.
+
+             c#  variable
+             int, double, char, string, bool
+             
+             
+             */
+
+            string name = "John";
+            int myAge = 45;
+             
+            Console.WriteLine("My Name is " + name + " I am " + myAge + " years old");
+
+            int x = 5;
+            int y = 6;
+            Console.WriteLine(x + y); // Print the value of x + y
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
